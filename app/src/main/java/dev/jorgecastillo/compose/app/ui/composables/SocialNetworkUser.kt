23df03c93e5eaeb481b94565b87fb62466eda5cc
Pranjal Desai <pranjal.p.d@gmail.com>
2024-04-06 11:42:35 -0500

    Updated exercise 2 with solution

--- app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/SocialNetworkUser.kt
+++ app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/SocialNetworkUser.kt
@@ -1,8 +1,11 @@
 package dev.jorgecastillo.compose.app.ui.composables
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -14,7 +17,17 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun SocialNetworkUser(name: String, location: String, onFollow: () -> Unit) {
-    
+    Box(modifier = Modifier.fillMaxSize()) {
+        Row(modifier = Modifier.padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
+            Column() {
+                Text(text = name)
+                Text(text = location)
+            }
+            Button(onClick = onFollow, Modifier.padding(start = 8.dp)) {
+                Text(text = "Follow")
+            }
+        }
+    }
 }
 
 @Composable
